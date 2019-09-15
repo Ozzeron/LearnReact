a590275a7cd1f293d4c572bde69bd4e27c861757
Ozzeron <Ozzeron@gmail.com>
2019-09-15 23:30:56 +0300

    Playing with props and onclick on addpost button

--- react-ezpz-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-ezpz-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import profiles from './MyPosts.module.css';
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-    return (<div className={profiles.posts}>
+const MyPosts = (props) => {
+    return (<div className={profiles.posts} id='MyPosts'>
             my posts
             <div>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
-                <button>Add post</button>
+                <textarea name="text" id="text" cols="30" rows="10"></textarea>
+                <button onClick={addPost}>Add post</button>
                 <button>Remove</button>
             </div>
-            <Post />
+            <div id='posts'>
+                <Post message='Hi, how are you' likesCount={15}/>
+                <Post message="It's my first post" likesCount={2}/>
+            </div>
         </div>
     );
 };
+
+function addPost() {
+    let text = document.getElementById('text').value;
+    console.log(text);
+}
 
 export default MyPosts;
--- react-ezpz-1/src/components/Profile/MyPosts/Post/Post.jsx
+++ react-ezpz-1/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import profiles from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={profiles.item}>
             <img src="https://picsum.photos/id/237/200/300" alt="avatar"/>
-            post 1
             <div>
-                <span>Like</span>
+                {props.message}
+            </div>
+            <div>
+                <span>Like</span> {props.likesCount}
             </div>
         </div>);
 };
