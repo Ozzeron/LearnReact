e25991ae633c9a943f9a006f15894604da705493
Ozzeron <Ozzeron@gmail.com>
2019-09-16 00:02:34 +0300

    add dialogs component

---
+++ react-ezpz-1/src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import dialogStyles from './Dialogs.module.css';
+
+const Dialogs = () => {
+    return (
+        <div className={dialogStyles.content}>
+            DIALOGS
+        </div>
+    );
+};
+
+export default Dialogs;
--- react-ezpz-1/src/components/Header/Header.jsx
+++ react-ezpz-1/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className={header.header}>
             <img alt={'header'}
-                src='https://www.designfreelogoonline.com/wp-content/uploads/2016/03/00106-3D-company-logo-design-free-logo-online-Template-03.png'/>
+                 src='https://www.designfreelogoonline.com/wp-content/uploads/2016/03/00106-3D-company-logo-design-free-logo-online-Template-03.png'/>
         </header>
 
     );
