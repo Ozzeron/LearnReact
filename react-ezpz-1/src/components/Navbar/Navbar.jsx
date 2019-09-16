294cb477a850017fc9d2f5811d24b4b203aae3ab
Nick <nikita.timohin@quardex.com>
2019-09-16 17:55:32 +0300

    some navbar fixes.

--- react-ezpz-1/src/components/Navbar/Navbar.jsx
+++ react-ezpz-1/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import classes from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div>
-                <a href="/Profile"><div>Profile</div></a>
+            <div className={classes.item}>
+                <NavLink to="/Profile">
+                    <div>Profile</div>
+                </NavLink>
             </div>
-            <div className={`${classes.active}`}>
-                <a href="/Dialogs"><div>Messages</div></a>
+            <div cclassName={classes.item}>
+                <NavLink to="/Dialogs" activeClassName={classes.activeLink}>
+                    <div>Messages</div>
+                </NavLink>
             </div>
-            <div>
-                <a href="/News"><div>News</div></a>
+            <div className={classes.item}>
+                <NavLink to="/News">
+                    <div>News</div>
+                </NavLink>
             </div>
-            <div>
-                <a href="/music"><div>Music</div></a>
+            <div className={classes.item}>
+                <NavLink to="/music">
+                    <div>Music</div>
+                </NavLink>
             </div>
-            <div>
-                <a href="/settings"><div>Settings</div></a>
+            <div className={classes.item}>
+                <NavLink to="/settings">
+                    <div>Settings</div>
+                </NavLink>
             </div>
         </nav>
     );
