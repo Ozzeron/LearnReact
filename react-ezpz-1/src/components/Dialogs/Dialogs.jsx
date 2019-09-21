16063f5d3f25fa5d1061a7ac8bfda8ad7b7b7684
Ozzeron <Ozzeron@gmail.com>
2019-09-22 00:08:35 +0300

    Don't use qery js methods! learning react ref method + onClick

--- react-ezpz-1/src/components/Dialogs/Dialogs.jsx
+++ react-ezpz-1/src/components/Dialogs/Dialogs.jsx
@@ -6,6 +6,13 @@ import Message from "./DialogsMessages/Message";
 const Dialogs = (props) => {
     let dialogsData = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messagesData = props.state.messages.map(m => <Message message={m.message}/>);
+
+    let messagetext = React.createRef()
+
+    let addMessage = () =>{
+      let message = messagetext.current.value;
+      alert(message)
+    };
     return (
         <div className={dialogStyles.content}>
             <div className={dialogStyles.dialogs}>
@@ -13,8 +20,13 @@ const Dialogs = (props) => {
                     {dialogsData}
                 </div>
                 <div className={dialogStyles.messages}>
+                    <div>
                     {messagesData}
+                    <textarea name="addmessage" ref={messagetext}  cols="30" rows="10"></textarea>
+                    </div>
+                    <button onClick={addMessage}>Add Message</button>
                 </div>
+
             </div>
         </div>
     );
--- react-ezpz-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-ezpz-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,12 +4,20 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
     let postsData = props.posts.map(p=><Post message={p.message} likesCount={p.likesCount}/>);
+
+    let newPostElement = React.createRef();
+
+    let addPost = ()=> {
+      let text = newPostElement.current.value;
+      console.log(text);
+    };
+
     return (<div className={profiles.posts} id='MyPosts'>
             my posts
             <div>
-                <textarea name="text" id="text" cols="30" rows="10"></textarea>
+                <textarea name="text" ref={newPostElement} cols="30" rows="10"></textarea>
                 <div className={profiles.buttons}>
-                    <button className="button" onClick={()=>{alert('hei')}}>Add post</button>
+                    <button className="button" onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div id='posts'>
