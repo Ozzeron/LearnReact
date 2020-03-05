365f277688654eaf3055c454b6aa2807d1f89763
Ozzeron <Ozzeron@gmail.com>
2020-03-05 21:17:14 +0200

    finish 61 lesson

--- react-ezpz-1/src/App.js
+++ react-ezpz-1/src/App.js
@@ -10,11 +10,12 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
     return (
         <div className="app-wrapper">
-            <Header/>
+            <HeaderContainer/>
             <NavbarContainer/>
             <div className='content'>
                 <Route path='/dialogs'
--- react-ezpz-1/src/components/Header/Header.jsx
+++ react-ezpz-1/src/components/Header/Header.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import header from './Header.module.css';
 import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
+    console.log(props)
     return (
         <header className={header.header}>
-            <NavLink to="/profile">
-                <img alt={'header'}
-                     src='https://www.designfreelogoonline.com/wp-content/uploads/2016/03/00106-3D-company-logo-design-free-logo-online-Template-03.png'/>
-            </NavLink>
+            <div>
+                <NavLink to="/profile">
+                    <img alt={'header'}
+                         src='https://www.designfreelogoonline.com/wp-content/uploads/2016/03/00106-3D-company-logo-design-free-logo-online-Template-03.png'/>
+                </NavLink>
+            </div>
             <div className={header.Hello}>
                 <div className={header.typewriter}>
                     <h1>Welcome.</h1>
                 </div>
+            </div>
+            <div id={header.login}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
             </div>
         </header>
 
